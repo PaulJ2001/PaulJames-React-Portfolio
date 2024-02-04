5c0c8cf34f4a3c00e207744eeda10102c58a3b69
PaulJ2001 <thejames007007@gmail.com>
2024-02-04 17:38:24 -0500

    Initial Internationalization components and functionality added

---
+++ pages/textComponents/Welcome.js
@@ -0,0 +1,16 @@
+
+import {useTranslation} from 'react-i18next';
+
+
+function Welcome(){
+    const {t} = useTranslation();
+
+    return(
+        <>
+        <h1>{t('welcome')}</h1>
+        <div>{t('language')}</div>
+        </>
+    )
+}
+
+export default Welcome;
