25a6e687716ae6daad35f08bc082ff1f372fc63e
PaulJ2001 <thejames007007@gmail.com>
2024-02-05 23:56:21 -0500

    set up page structure and responsiveness

--- pages/components/Banner.jsx
+++ pages/components/Banner.jsx
@@ -1,31 +1,79 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-import profileImg from "../public/ProfileImg.jpg";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Banner = () => {
     return (
-        <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center"></div>
-                <h1 className="mb-4 text-4xl lg:text-6xl font-extrabold text-blue-800 dark:text-white">
-                    Paul James
-                </h1>
-                <h2 className="text-2xl lg:text-4xl text-blue-900 dark:text-white">
-                    Developer and IT specialist
-                </h2>
-                <p className="text-xl lg:text-2xl mb-6 text-blue-950 dark:text-white">
-                    22 Year old Computer Scientist with an interest in IT hardware
-                    and backend development
-                </p>
-            </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <div className="rounded-full bg-blue-950 w-[250px] h-[250x] lg:w-[400px] lg:h-[400px] relative">
-                    <Image src={profileImg} alt="ProfileImg" 
-                    className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 " 
-                    width={300} height={300}
-                    />
-                </div>
-            </div>
+        <section className="lg:py-16">
+          <div className="grid grid-cols-1 sm:grid-cols-12">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+            >
+              <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+                  Hello, I&apos;m{" "}
+                </span>
+                <br></br>
+                <TypeAnimation
+                  sequence={[
+                    "Judy",
+                    1000,
+                    "Web Developer",
+                    1000,
+                    "Mobile Developer",
+                    1000,
+                    "UI/UX Designer",
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={50}
+                  repeat={Infinity}
+                />
+              </h1>
+              <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+                voluptuous.
+              </p>
+              <div>
+                <Link
+                  href="/#contact"
+                  className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+                >
+                  Hire Me
+                </Link>
+                <Link
+                  href="/"
+                  className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+                >
+                  <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                    Download CV
+                  </span>
+                </Link>
+              </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="col-span-4 place-self-center mt-4 lg:mt-0"
+            >
+              <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                <Image
+                  src="/images/atlas-background.jpg"
+                  alt="hero image"
+                  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  width={300}
+                  height={300}
+                />
+              </div>
+            </motion.div>
+          </div>
         </section>
     );
 };
