af09821c5b3b456b13c12bfce07751f500045cb1
PaulJ2001 <thejames007007@gmail.com>
2024-05-14 21:00:38 -0400

    Improved Skills Section CSS

--- pages/components/EmailSection.jsx
+++ pages/components/EmailSection.jsx
@@ -14,7 +14,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const form = useRef();
   const [emailSubmitted, setEmailSubmitted] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
@@ -55,7 +55,8 @@ const EmailSection = () => {
           {/* Let&apos;s Connect */}
         </h5>
         <div className="mt-12">
-  <a href="https://www.dropbox.com/scl/fi/td3qrg3uyk92boflyu0u0/PaulJames_English_IT_Resume_PDFVersion.pdf?dl=1">
+        <a href={i18n.language === 'fr' ? "https://www.dropbox.com/scl/fi/pf7soi838w8524d6j10y6/PaulJames_IT_Technician_Resume_Fr_PDF.pdf?rlkey=rc899a5gyllitfbfm6rkrpyqv&dl=1" : "https://www.dropbox.com/scl/fi/td3qrg3uyk92boflyu0u0/PaulJames_English_IT_Resume_PDFVersion.pdf?dl=1"}>
+  {/* <a href="https://www.dropbox.com/scl/fi/td3qrg3uyk92boflyu0u0/PaulJames_English_IT_Resume_PDFVersion.pdf?dl=1"> */}
     <button className="bg-primary-500 border border-white hover:bg-primary-600 hover:border-primary-600 text-white font-medium py-2.5 px-5 rounded-lg">
       {t('emailSection.cvDownloadLabel')}
     </button>
@@ -109,14 +110,6 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        {/* {emailSubmitted ? (
-            <div className="notification">
-            <p className="text-green-500 text-sm mt-2">Email sent successfully!</p>
-          </div>
-          ) : ( */}
-        {/* {showNotification && (
-          <p className="text-green-500 text-sm mt-2">Email sent successfully!</p>
-        )} */}
         <form className="flex flex-col" ref={form} onSubmit={sendEmail}>
           <div className="mb-6">
             <label
@@ -189,7 +182,6 @@ const EmailSection = () => {
             {t('emailSection.sendEmailLabel')}
           </button>
         </form>
-        {/* )} */}
         {showNotification && (
           <div className="fixed bottom-0 right-0 mb-8 mr-8 bg-green-500 text-white py-2 px-4 rounded-lg">
             {t('emailSection.sentNotificationMsg')}
@@ -201,175 +193,3 @@ const EmailSection = () => {
 };
 
 export default EmailSection;
-
-// const EmailSection = () => {
-//   const [emailSubmitted, setEmailSubmitted] = useState(false);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const data = {
-//       email: e.target.email.value,
-//       subject: e.target.subject.value,
-//       message: e.target.message.value,
-//     };
-//     const JSONdata = JSON.stringify(data);
-//     const endpoint = "/api/send";
-
-//     // Form the request for sending data to the server.
-//     const options = {
-//       // The method is POST because we are sending data.
-//       method: "POST",
-//       // Tell the server we're sending JSON.
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       // Body of the request is the JSON data we created above.
-//       body: JSONdata,
-//     };
-
-//     const response = await fetch(endpoint, options);
-//     const resData = await response.json();
-
-//     if (response.status === 200) {
-//       console.log("Message sent.");
-//       setEmailSubmitted(true);
-//     }
-//   };
-
-//   return (
-//     <section
-//       id="contact"
-//       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-//     >
-//       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-//       <div className="z-10">
-//         <h5 className="text-xl font-bold text-white my-2">
-//           Let&apos;s Connect
-//         </h5>
-//         {/* <p className="text-[#ADB7BE] mb-4 max-w-md">
-//           {" "}
-//           I&apos;m currently looking for new opportunities, my inbox is always
-//           open. Whether you have a question or just want to say hi, I&apos;ll
-//           try my best to get back to you!
-//         </p> */}
-//         <div className="socials flex flex-row gap-2">
-//           {/* <Link href="github.com">
-//             <Image src={GithubIcon} alt="Github Icon" />
-//           </Link>
-//           <Link href="linkedin.com">
-//             <Image src={LinkedinIcon} alt="Linkedin Icon" />
-//           </Link> */}
-//           <div className="py-10 mt-12 text-3xl items-center justify-between md:text-5xl flex flex-wrap lg:flex-row lg:flex-wrap text-white dark:text-blue-950 ">
-//               <a
-//                 href="https://github.com/PaulJ2001"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <AiFillGithub />
-//               </a>
-//               <a
-//                 href="https://www.linkedin.com/in/paul-james-pj/"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <AiFillLinkedin />
-//               </a>
-//               <a
-//                 href="https://www.youtube.com/channel/UCl6mxqMh5pGKK9s1uBc9cZg"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <AiFillYoutube />
-//               </a>
-//               <a
-//                 href="https://www.facebook.com/people/Paul-James/pfbid0xtf45Lwydsoue93UPHuCBzHXoNX9NuNhc2ggoGeZg8bAYMtYL7hnEgncS7EopTyVl/"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <AiFillFacebook />
-//               </a>
-//               <a
-//                 href="https://www.instagram.com/pauljamespj2001/"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <AiFillInstagram />
-//               </a>
-//               <a
-//                 href="https://twitter.com/PaulJam43765527"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//               >
-//                 <AiFillTwitterCircle />
-//               </a>
-//               {/* <FontAwesomeIcon icon={faSquareXTwitter} /> */}
-//             </div>
-//         </div>
-//       </div>
-//       <div>
-//         {emailSubmitted ? (
-//           <p className="text-green-500 text-sm mt-2">
-//             Email sent successfully!
-//           </p>
-//         ) : (
-//           <form className="flex flex-col" onSubmit={handleSubmit}>
-//             <div className="mb-6">
-//               <label
-//                 htmlFor="email"
-//                 className="text-white block mb-2 text-sm font-medium"
-//               >
-//                 Your email
-//               </label>
-//               <input
-//                 name="email"
-//                 type="email"
-//                 id="email"
-//                 required
-//                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-//                 placeholder="youremail@email.com"
-//               />
-//             </div>
-//             <div className="mb-6">
-//               <label
-//                 htmlFor="subject"
-//                 className="text-white block text-sm mb-2 font-medium"
-//               >
-//                 Subject
-//               </label>
-//               <input
-//                 name="subject"
-//                 type="text"
-//                 id="subject"
-//                 required
-//                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-//                 placeholder="Just saying hi"
-//               />
-//             </div>
-//             <div className="mb-6">
-//               <label
-//                 htmlFor="message"
-//                 className="text-white block text-sm mb-2 font-medium"
-//               >
-//                 Message
-//               </label>
-//               <textarea
-//                 name="message"
-//                 id="message"
-//                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-//                 placeholder="Let's talk about..."
-//               />
-//             </div>
-//             <button
-//               type="submit"
-//               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-//             >
-//               Send Message
-//             </button>
-//           </form>
-//         )}
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default EmailSection;
--- pages/components/SkillsSection.jsx
+++ pages/components/SkillsSection.jsx
@@ -5,13 +5,29 @@ const SkillsSection = () => {
   const { t } = useTranslation();
 
   const SkillCategory = ({ title, skills }) => {
+    const midpoint = Math.ceil(skills.length / 2);
+    const leftColumn = skills.slice(0, midpoint);
+    const rightColumn = skills.slice(midpoint);
+
     return (
       <div>
         <h3 className="text-xl font-semibold text-white mb-4">{title}</h3>
-        <ul className="text-white">
-          {skills.map((skill, index) => ( <li key={index}>{skill}</li>
-          ))}
-        </ul>
+        <div className="grid grid-cols-2 gap-4">
+          <ul className="text-white">
+            {leftColumn.map((skill, index) => (
+              <li key={index}>
+                <span className="inline-block mr-1">&#8226;</span>{skill}
+              </li>
+            ))}
+          </ul>
+          <ul className="text-white">
+            {rightColumn.map((skill, index) => (
+              <li key={index}>
+                <span className="inline-block mr-1">&#8226;</span>{skill}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   };
@@ -25,7 +41,7 @@ const SkillsSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
             <SkillCategory
-              title= {t('skillsSection.languagesFrameworksTitle').split(', ')}
+              title= {t('skillsSection.languagesFrameworksTitle')}
               skills={[
                 ...t('skillsSection.languagesFrameworksProficient').split(', '),
                 ...t('skillsSection.languagesFrameworksFamiliar').split(', ')
@@ -34,109 +50,115 @@ const SkillsSection = () => {
           </div>
           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
             <SkillCategory
-              title= {t('skillsSection.itToolsTitle').split(', ')}
-              skills={
-                t('skillsSection.itToolsList').split(', ')}
+              title= {t('skillsSection.itToolsTitle')}
+              skills={t('skillsSection.itToolsList').split(', ')}
             />
           </div>
           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
             <SkillCategory
-              title= {t('skillsSection.dbManagementTitle').split(', ')}
+              title= {t('skillsSection.dbManagementTitle')}
               skills={t('skillsSection.dbManagementList').split(', ')}
             />
           </div>
-          {/* <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
-            <SkillCategory
-              title="Operating Systems"
-              skills={["Windows 11/10", "Linux (on Windows Virtual Machine)", "macOS"]}
-            />
-          </div> */}
           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
             <SkillCategory
-              title= {t('skillsSection.interpersonalSkillsTitle').split(', ')}
+              title= {t('skillsSection.interpersonalSkillsTitle')}
               skills={t('skillsSection.interpersonalSkillsList').split(', ')}
             />
           </div>
           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
             <SkillCategory
-              title={t('skillsSection.financeAccountingTitle').split(', ')}
+              title={t('skillsSection.financeAccountingTitle')}
               skills= {t('skillsSection.financeAccountingList').split(', ')}
-                      
-              />
-            </div>
-            <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
-              <SkillCategory
-                title= {t('skillsSection.itSupportTitle').split(', ')}
-                skills={
-                  t('skillsSection.itSupportList').split(', ')
-                  }
-                />
-              </div>
+            />
+          </div>
+          <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+            <SkillCategory
+              title= {t('skillsSection.itSupportTitle')}
+              skills={t('skillsSection.itSupportList').split(', ')}
+            />
+          </div>
         </div>
       </div>
     </section>
   );
 };
 
-
-
 export default SkillsSection;
 
+
+
+
+// import React from "react";
+// import { useTranslation } from 'react-i18next'; 
+
+// const SkillsSection = () => {
+//   const { t } = useTranslation();
+
+//   const SkillCategory = ({ title, skills }) => {
+//     return (
+//       <div>
+//         <h3 className="text-xl font-semibold text-white mb-4">{title}</h3>
+//         <ul className="text-white">
+//           {skills.map((skill, index) => ( <li key={index}>{skill}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   };
+  
 //   return (
-//     <section className="py-16 bg-[#121212]">
+//     <section id="skills" className="py-16 bg-[#121212]">
 //       <div className="container mx-auto px-4">
 //         <h2 className="text-4xl text-center text-white font-bold mb-8">
-//           My Skills
+//           {t('skillsSection.title')}
 //         </h2>
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-//           <div className="bg-[#181818] p-6 rounded-lg">
-//             <h3 className="text-xl font-semibold text-white mb-4">
-//               Proficient
-//             </h3>
-//             <ul className="text-white">
-//               <li>SQL</li>
-//               <li>Java</li>
-//               <li>Swift</li>
-//               <li>HTML</li>
-//               <li>Angular</li>
-//               <li>React</li>
-//               <li>CSS</li>
-//               <li>JS</li>
-//               <li>C#</li>
-//             </ul>
+//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+//           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+//             <SkillCategory
+//               title= {t('skillsSection.languagesFrameworksTitle').split(', ')}
+//               skills={[
+//                 ...t('skillsSection.languagesFrameworksProficient').split(', '),
+//                 ...t('skillsSection.languagesFrameworksFamiliar').split(', ')
+//               ]}
+//             />
 //           </div>
-//           <div className="bg-[#181818] p-6 rounded-lg">
-//             <h3 className="text-xl font-semibold text-white mb-4">Familiar</h3>
-//             <ul className="text-white">
-//               <li>ASP.NET</li>
-//               <li>Python</li>
-//               <li>AJAX</li>
-//               <li>PHP</li>
-//               <li>C++</li>
-//               <li>NodeJS</li>
-//             </ul>
+//           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+//             <SkillCategory
+//               title= {t('skillsSection.itToolsTitle').split(', ')}
+//               skills={
+//                 t('skillsSection.itToolsList').split(', ')}
+//             />
 //           </div>
-//           <div className="bg-[#181818] p-6 rounded-lg">
-//             <h3 className="text-xl font-semibold text-white mb-4">Tools</h3>
-//             <ul className="text-white">
-//               <li>Eclipse Netbeans</li>
-//               <li>MS Visual Studio</li>
-//               <li>Postman</li>
-//               <li>Lucidchart</li>
-//               <li>XCode</li>
-//               <li>PyCharm</li>
-//               <li>Jira</li>
-//               <li>Figma</li>
-//               <li>Spring</li>
-//               <li>IntelliJ IDEA</li>
-//               <li>Git</li>
-//               <li>Github</li>
-//               <li>Docker</li>
-//             </ul>
+//           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+//             <SkillCategory
+//               title= {t('skillsSection.dbManagementTitle').split(', ')}
+//               skills={t('skillsSection.dbManagementList').split(', ')}
+//             />
 //           </div>
+//           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+//             <SkillCategory
+//               title= {t('skillsSection.interpersonalSkillsTitle').split(', ')}
+//               skills={t('skillsSection.interpersonalSkillsList').split(', ')}
+//             />
+//           </div>
+//           <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+//             <SkillCategory
+//               title={t('skillsSection.financeAccountingTitle').split(', ')}
+//               skills= {t('skillsSection.financeAccountingList').split(', ')}
+//               />
+//             </div>
+//             <div className="bg-[#181818] p-6 rounded-lg flex flex-col">
+//               <SkillCategory
+//                 title= {t('skillsSection.itSupportTitle').split(', ')}
+//                 skills={
+//                   t('skillsSection.itSupportList').split(', ')
+//                   }
+//                 />
+//               </div>
 //         </div>
 //       </div>
 //     </section>
 //   );
 // };
-
+// export default SkillsSection;
